8f930bd001ecef6d735c95a5254e407a36b389d0
nikunj0607 <nikunjparmar1986@gmail.com>
2025-10-23 23:04:55 +0530

    Refactor start.sh to simplify server startup
    Removed error handling and sleep delay, simplified server start.

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-#!/usr/bin/env bash
-set -e
-
-# Start keepalive web server in background
-gunicorn keepalive:app --bind 0.0.0.0:${PORT} &
-
-# Delay 2 seconds to make sure webserver boots
-sleep 2
-
-# Start the bot loop
-python bot.py
+#!/bin/bash
+gunicorn bot:app --bind 0.0.0.0:$PORT
