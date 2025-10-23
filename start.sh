8cd2ef4d7ae2818d7f8a62347e4940bc5e7a261f
nikunj0607 <nikunjparmar1986@gmail.com>
2025-10-23 21:22:11 +0530

    Add delay before starting the bot
    Added a delay before starting the bot to ensure the web server is fully booted.

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
-# Start mini web server to keep Render awake
+
+# Start keepalive web server in background
 gunicorn keepalive:app --bind 0.0.0.0:${PORT} &
 
-# Start the bot
+# Delay 2 seconds to make sure webserver boots
+sleep 2
+
+# Start the bot loop
 python bot.py
